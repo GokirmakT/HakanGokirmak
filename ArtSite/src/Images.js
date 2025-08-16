abf921aab4f5273d1ec8cc05b33dc8724b3afc62
GokirmakT <gokirmak.teoman@gmail.com>
2025-08-17 00:29:24 +0300

    animasyon süreleri

--- ArtSite/src/Images.js
+++ ArtSite/src/Images.js
@@ -1,167 +1,60 @@
- const images = [
-    {
-      id:1,
-      src: '/src/assets/sanat1.jpg',
-      src2: '/src/assets/sanat1-2.jpeg',
-      src3: '/src/assets/salon3.png',
-      title: ' BU DÜNYANIN KRALLIĞI',
-      description: 'Bu ilk resim açıklaması.',
-      size: '1920x1080',
-      date: '14 EYLÜL 2024',
-    },
-    {
-      id:2,
-      src: '/src/assets/resim2.jpeg',
-      src2: '/src/assets/resim2-2.jpeg',
-      title: 'BU DÜNYANIN KRALLIĞI',
-      description: 'Bu ilk resim açıklaması.',
-      size: '1920x1080',
-      date: '14 EYLÜL 2024',
-    },
-    {
-      id:3,
-      src: '/src/assets/resim3.jpeg',
-      src2: '/src/assets/resim3-2.jpeg',
-      title: 'BU DÜNYANIN KRALLIĞI',
-      description: 'Bu ilk resim açıklaması.',
-      size: '1920x1080',
-      date: '14 EYLÜL 2024',
-    },
-    {
-      id:4,
-      src: '/src/assets/resim4.jpeg',
-      src2: '/src/assets/resim4-2.jpeg',
-      title: 'BU DÜNYANIN KRALLIĞI',
-      description: 'Bu ilk resim açıklaması.',
-      size: '1920x1080',
-      date: '14 EYLÜL 2024',
-    },
-    {
-      id:5,
-      src: '/src/assets/resim5.jpeg',
-      src2: '/src/assets/resim5-2.jpeg',
-      title: 'BU DÜNYANIN KRALLIĞI',
-      description: 'Bu ilk resim açıklaması.',
-      size: '1920x1080',
-      date: '14 EYLÜL 2024',
-    },
-    {
-      id:6,
-      src: '/src/assets/resim6.jpeg',
-      src2: '/src/assets/resim6-2.jpeg',
-      title: 'BU DÜNYANIN KRALLIĞI',
-      description: 'Bu ilk resim açıklaması.',
-      size: '1920x1080',
-      date: '14 EYLÜL 2024',
-    },
-    {
-      id:7,
-      src: '/src/assets/resim7.jpeg',
-      src2: '/src/assets/resim7-2.jpeg',
-      title: 'BU DÜNYANIN KRALLIĞI',
-      description: 'Bu ilk resim açıklaması.',
-      size: '1920x1080',
-      date: '14 EYLÜL 2024',
-    },
-    {
-      id:8,
-      src: '/src/assets/sanat8.jpeg',
-      src2: '/src/assets/sanat8-2.jpeg',
-      title: 'BU DÜNYANIN KRALLIĞI',
-      description: 'Bu ilk resim açıklaması.',
-      size: '1920x1080',
-      date: '14 EYLÜL 2024',
-    },
-    {
-      id:9,
-      src: '/src/assets/resim9.jpeg',
-      src2: '/src/assets/resim9-2.jpeg',
-      title: 'BU DÜNYANIN KRALLIĞI',
-      description: 'Bu ilk resim açıklaması.',
-      size: '1920x1080',
-      date: '14 EYLÜL 2024',
-    },
-    {
-      id:10,
-      src: '/src/assets/resim10.jpeg',
-      src2: '/src/assets/resim10-2.jpeg',
-      title: 'BU DÜNYANIN KRALLIĞI',
-      description: 'Bu ilk resim açıklaması.',
-      size: '1920x1080',
-      date: '14 EYLÜL 2024',
-    },
-    {
-      id:11,
-      src: '/src/assets/resim11.jpeg',
-      src2: '/src/assets/resim11-2.jpeg',
-      title: 'BU DÜNYANIN KRALLIĞI',
-      description: 'Bu ilk resim açıklaması.',
-      size: '1920x1080',
-      date: '14 EYLÜL 2024',
-    },
-    {
-      id:12,
-      src: '/src/assets/resim12.jpeg',
-      src2: '/src/assets/resim12-2.jpeg',
-      title: 'BU DÜNYANIN KRALLIĞI',
-      description: 'Bu ilk resim açıklaması.',
-      size: '1920x1080',
-      date: '14 EYLÜL 2024',
-    },
-    {
-      id:13,
-      src: '/src/assets/resim13.jpeg',
-      src2: '/src/assets/resim13-2.jpeg',
-      title: 'BU DÜNYANIN KRALLIĞI',
-      description: 'Bu ilk resim açıklaması.',
-      size: '1920x1080',
-      date: '14 EYLÜL 2024',
-    },
-    {
-      id:14,
-      src: '/src/assets/resim14.jpeg',
-      src2: '/src/assets/resim14-2.jpeg',
-      title: 'BU DÜNYANIN KRALLIĞI',
-      description: 'Bu ilk resim açıklaması.',
-      size: '1920x1080',
-      date: '14 EYLÜL 2024',
-    },
-    {
-      id:15,
-      src: '/src/assets/resim15.jpeg',
-      src2: '/src/assets/resim15-2.jpeg',
-      title: 'BU DÜNYANIN KRALLIĞI',
-      description: 'Bu ilk resim açıklaması.',
-      size: '1920x1080',
-      date: '14 EYLÜL 2024',
-    },
-    {
-      id:16,
-      src: '/src/assets/resim16.jpeg',
-      src2: '/src/assets/resim16-2.jpeg',
-      title: 'BU DÜNYANIN KRALLIĞI',
-      description: 'Bu ilk resim açıklaması.',
-      size: '1920x1080',
-      date: '14 EYLÜL 2024',
-    },
-    {
-      id:17,
-      src: '/src/assets/resim17.jpeg',
-      src2: '/src/assets/resim17-2.jpeg',
-      title: 'BU DÜNYANIN KRALLIĞI',
-      description: 'Bu ilk resim açıklaması.',
-      size: '1920x1080',
-      date: '14 EYLÜL 2024',
-    },
-    {
-      id:18,
-      src: '/src/assets/resim18.jpeg',
-      src2: '/src/assets/resim18-2.jpeg',
-      title: 'BU DÜNYANIN KRALLIĞI',
-      description: 'Bu ilk resim açıklaması.',
-      size: '1920x1080',
-      date: '14 EYLÜL 2024',
-    }
-  ];
+import resim1 from './assets/sanat1.jpg';
+import resim1_2 from './assets/sanat1-2.jpeg';
+import resim2 from './assets/resim2.jpeg';
+import resim2_2 from './assets/resim2-2.jpeg';
+import resim3 from './assets/resim3.jpeg';
+import resim3_2 from './assets/resim3-2.jpeg';
+import resim4 from './assets/resim4.jpeg';
+import resim4_2 from './assets/resim4-2.jpeg';
+import resim5 from './assets/resim5.jpeg';
+import resim5_2 from './assets/resim5-2.jpeg';
+import resim6 from './assets/resim6.jpeg';
+import resim6_2 from './assets/resim6-2.jpeg';
+import resim7 from './assets/resim7.jpeg';
+import resim7_2 from './assets/resim7-2.jpeg';
+import resim8 from './assets/sanat8.jpeg';
+import resim8_2 from './assets/sanat8-2.jpeg';
+import resim9 from './assets/resim9.jpeg';
+import resim9_2 from './assets/resim9-2.jpeg';
+import resim10 from './assets/resim10.jpeg';
+import resim10_2 from './assets/resim10-2.jpeg';
+import resim11 from './assets/resim11.jpeg';
+import resim11_2 from './assets/resim11-2.jpeg';
+import resim12 from './assets/resim12.jpeg';
+import resim12_2 from './assets/resim12-2.jpeg';
+import resim13 from './assets/resim13.jpeg';
+import resim13_2 from './assets/resim13-2.jpeg';
+import resim14 from './assets/resim14.jpeg';
+import resim14_2 from './assets/resim14-2.jpeg';
+import resim15 from './assets/resim15.jpeg';
+import resim15_2 from './assets/resim15-2.jpeg';
+import resim16 from './assets/resim16.jpeg';
+import resim16_2 from './assets/resim16-2.jpeg';
+import resim17 from './assets/resim17.jpeg';
+import resim17_2 from './assets/resim17-2.jpeg';
+import resim18 from './assets/resim18.jpeg';
+import resim18_2 from './assets/resim18-2.jpeg';
+import salon3 from './assets/salon3.png';
 
-  export default images;
+const images = [
+  { id: 1, src: resim1, src2: resim1_2, src3: salon3, title: 'BU DÜNYANIN KRALLIĞI', description: 'Bu ilk resim açıklaması.', size: '1920x1080', date: '14 EYLÜL 2024' },
+  { id: 2, src: resim2, src2: resim2_2, title: 'BU DÜNYANIN KRALLIĞI', description: 'Bu ikinci resim açıklaması.', size: '1920x1080', date: '14 EYLÜL 2024' },
+  { id: 3, src: resim3, src2: resim3_2, title: 'BU DÜNYANIN KRALLIĞI', description: 'Bu üçüncü resim açıklaması.', size: '1920x1080', date: '14 EYLÜL 2024' },
+  { id: 4, src: resim4, src2: resim4_2, title: 'BU DÜNYANIN KRALLIĞI', description: 'Bu dördüncü resim açıklaması.', size: '1920x1080', date: '14 EYLÜL 2024' },
+  { id: 5, src: resim5, src2: resim5_2, title: 'BU DÜNYANIN KRALLIĞI', description: 'Bu beşinci resim açıklaması.', size: '1920x1080', date: '14 EYLÜL 2024' },
+  { id: 6, src: resim6, src2: resim6_2, title: 'BU DÜNYANIN KRALLIĞI', description: 'Bu altıncı resim açıklaması.', size: '1920x1080', date: '14 EYLÜL 2024' },
+  { id: 7, src: resim7, src2: resim7_2, title: 'BU DÜNYANIN KRALLIĞI', description: 'Bu yedinci resim açıklaması.', size: '1920x1080', date: '14 EYLÜL 2024' },
+  { id: 8, src: resim8, src2: resim8_2, title: 'BU DÜNYANIN KRALLIĞI', description: 'Bu sekizinci resim açıklaması.', size: '1920x1080', date: '14 EYLÜL 2024' },
+  { id: 9, src: resim9, src2: resim9_2, title: 'BU DÜNYANIN KRALLIĞI', description: 'Bu dokuzuncu resim açıklaması.', size: '1920x1080', date: '14 EYLÜL 2024' },
+  { id: 10, src: resim10, src2: resim10_2, title: 'BU DÜNYANIN KRALLIĞI', description: 'Bu onuncu resim açıklaması.', size: '1920x1080', date: '14 EYLÜL 2024' },
+  { id: 11, src: resim11, src2: resim11_2, title: 'BU DÜNYANIN KRALLIĞI', description: 'Bu on birinci resim açıklaması.', size: '1920x1080', date: '14 EYLÜL 2024' },
+  { id: 12, src: resim12, src2: resim12_2, title: 'BU DÜNYANIN KRALLIĞI', description: 'Bu on ikinci resim açıklaması.', size: '1920x1080', date: '14 EYLÜL 2024' },
+  { id: 13, src: resim13, src2: resim13_2, title: 'BU DÜNYANIN KRALLIĞI', description: 'Bu on üçüncü resim açıklaması.', size: '1920x1080', date: '14 EYLÜL 2024' },
+  { id: 14, src: resim14, src2: resim14_2, title: 'BU DÜNYANIN KRALLIĞI', description: 'Bu on dördüncü resim açıklaması.', size: '1920x1080', date: '14 EYLÜL 2024' },
+  { id: 15, src: resim15, src2: resim15_2, title: 'BU DÜNYANIN KRALLIĞI', description: 'Bu on beşinci resim açıklaması.', size: '1920x1080', date: '14 EYLÜL 2024' },
+  { id: 16, src: resim16, src2: resim16_2, title: 'BU DÜNYANIN KRALLIĞI', description: 'Bu on altıncı resim açıklaması.', size: '1920x1080', date: '14 EYLÜL 2024' },
+  { id: 17, src: resim17, src2: resim17_2, title: 'BU DÜNYANIN KRALLIĞI', description: 'Bu on yedinci resim açıklaması.', size: '1920x1080', date: '14 EYLÜL 2024' },
+  { id: 18, src: resim18, src2: resim18_2, title: 'BU DÜNYANIN KRALLIĞI', description: 'Bu on sekizinci resim açıklaması.', size: '1920x1080', date: '14 EYLÜL 2024' },
+];
+
+export default images;
